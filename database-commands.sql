3560c6105bc491640a632cff0ffca3a0cf74a48a
Jordan <jordan@chude.co>
2022-03-11 00:09:05 -0500

    part six

--- database-commands.sql
+++ database-commands.sql
@@ -24,3 +24,29 @@ ADD category TEXT;
 
 ALTER TABLE films 
 ADD rating DECIMAL(2,1);
+
+-- Part Five - Updating Data
+UPDATE films
+SET runtime = 103, 
+		category = 'drama', 
+    rating = 8.2, 
+    box_office_earnings = 264000000
+WHERE name = 'The Truman Show';
+
+UPDATE films
+SET runtime = 136, 
+		category = 'action', 
+    rating = 8.7, 
+    box_office_earnings = 466000000
+WHERE name = 'The Matrix';
+
+UPDATE films
+SET runtime = 142, 
+		category = 'drama', 
+    rating = 9.3, 
+    box_office_earnings = 73000000
+WHERE name = 'The Shawshank Redemption';
+
+-- Part Six - Adding Constraints
+ALTER TABLE films
+ADD CONSTRAINT unique_name UNIQUE (name);
