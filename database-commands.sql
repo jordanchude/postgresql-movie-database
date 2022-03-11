756c4c7a6cd380f13d10b2e0d924bd21ccc0fb29
Jordan <jordan@chude.co>
2022-03-10 23:38:34 -0500

    created table and added films

---
+++ database-commands.sql
@@ -0,0 +1,12 @@
+-- Part One - Creating Tables
+CREATE TABLE films (
+	name TEXT,
+  release_year INTEGER
+);
+
+-- Part Two - Inserting Data
+INSERT INTO films (name, release_year)
+VALUES 
+  ('The Truman Show', 1998),
+  ('The Matrix', 1999),
+  ('The Shawshank Redemption', 1994);
